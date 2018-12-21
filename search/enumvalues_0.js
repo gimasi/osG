87439348a145df19526e0537219699360dcbbb8d
Francesco Stefanni <francesco.stefanni@edalab.it>
2018-12-21 11:56:42 +0100

    Updated doxygen

--- search/enumvalues_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['osg_5fspi_5fslave_5fselect_5fauto',['OSG_SPI_SLAVE_SELECT_AUTO',['../group__SPI.html#ggad2f1b0b369b97c3f127fba11c60aad54afed117962af49629072db4c4616e3a66',1,'SpiDataTypes.h']]],
-  ['osg_5fspi_5fslave_5fselect_5finput',['OSG_SPI_SLAVE_SELECT_INPUT',['../group__SPI.html#ggad2f1b0b369b97c3f127fba11c60aad54a1294a9559ceda671f1082a1528483886',1,'SpiDataTypes.h']]],
-  ['osg_5fspi_5fslave_5fselect_5foutput',['OSG_SPI_SLAVE_SELECT_OUTPUT',['../group__SPI.html#ggad2f1b0b369b97c3f127fba11c60aad54ae52398caceb688d7db380622fd7e01b3',1,'SpiDataTypes.h']]],
-  ['osg_5fspi_5fslave_5fselect_5funused',['OSG_SPI_SLAVE_SELECT_UNUSED',['../group__SPI.html#ggad2f1b0b369b97c3f127fba11c60aad54af2a2631dfd16c6e9be1ba2a3b1cce970',1,'SpiDataTypes.h']]]
-];
